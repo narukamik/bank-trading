516054de966a6d4eab95ec17295a82a582ff24eb
danilojneon <133019675+danilojneon@users.noreply.github.com>
2024-04-24 00:39:10 -0300

    Organizes repo's folder structure

---
+++ Second Day - Question 1/Domain/Enum/ClientSector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank_trading.Domain.Enum
+{
+    public enum ClientSector
+    {
+        Public,
+        Private
+    }
+}
---
+++ Second Day - Question 1/Infra/Services/TradeService.cs
@@ -0,0 +1,20 @@
+﻿using bank_trading.Domain;
+using bank_trading.Domain.Enum;
+
+namespace bank_trading.Infra.Services
+{
+    public class TradeService
+    {
+        public async Task<IEnumerable<Trade>> GetTradesAsync()
+        {
+            var trades = new List<Trade>
+            {
+                new Trade { Value = 2000000, ClientSector = ClientSector.Private },
+                new Trade { Value = 400000, ClientSector = ClientSector.Public },
+                new Trade { Value = 500000, ClientSector = ClientSector.Public }
+            };
+
+            return trades;
+        }
+    }
+}
